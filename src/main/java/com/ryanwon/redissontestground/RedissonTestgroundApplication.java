c2233cc2b585f8bfadb0d346df9c537391c11205
wlwlwon <ryanwon@naver.com>
2022-01-31 23:12:48 +0900

    start

---
+++ src/main/java/com/ryanwon/redissontestground/RedissonTestgroundApplication.java
@@ -0,0 +1,13 @@
+package com.ryanwon.redissontestground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonTestgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonTestgroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryanwon/redissontestground/RedissonTestgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanwon.redissontestground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonTestgroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
